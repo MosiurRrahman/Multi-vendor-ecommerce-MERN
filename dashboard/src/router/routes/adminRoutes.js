eb8bb19fbb7989f6d9e6d17c1b664b162e3a74a4
MosiurRrahman <mosiurrahman595@gmail.com>
2023-08-03 18:40:09 +0600

    sellers and payment request ad

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -5,6 +5,9 @@ const Sellers = lazy(()=>import("../../views/admin/Sellers"))
 const AdminDashboard = lazy(()=>import("../../views/admin/AdminDashboard"))
 const Orders = lazy(()=>import("../../views/admin/Orders"))
 const PaymentRequest = lazy(()=>import("../../views/admin/PaymentRequest"))
+const SellerRequest = lazy(()=>import("../../views/admin/SellerRequest"))
+const DeactiveSellers = lazy(()=>import("../../views/admin/DeactiveSellers"))
+const SellerDetails = lazy(()=>import("../../views/admin/SellerDetails"))
 
 export const adminRoutes = [
     {
@@ -33,6 +36,23 @@ export const adminRoutes = [
     {
         path:"admin/dashboard/payment-request",
         element: <PaymentRequest/>,
+        ability:"admin"
+    }
+    ,
+    {
+        path:"admin/dashboard/deactive-sellers",
+        element: <DeactiveSellers/>,
+        ability:"admin"
+    }
+    ,
+    {
+        path:"admin/dashboard/sellers-request",
+        element: <SellerRequest/>,
+        ability:"admin"
+    },
+    {
+        path:"admin/dashboard/seller/details/1",
+        element: <SellerDetails/>,
         ability:"admin"
     }
 ]
