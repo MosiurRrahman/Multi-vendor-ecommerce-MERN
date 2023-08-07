5590da48985de37f6cf5a37a5b93c76c25b22c1f
MosiurRrahman <mosiurrahman595@gmail.com>
2023-08-07 12:27:54 +0600

    create file and some jsx sellerto admin and admin to seller profile order details

--- dashboard/src/router/routes/sellerRoutes.js
+++ dashboard/src/router/routes/sellerRoutes.js
@@ -1,28 +1,46 @@
-
-import { lazy } from "react";
-
-const Home = lazy(()=>import("../../views/Home"))
-const SellerDashboard = lazy(()=>import("../../views/seller/SellerDashboard"))
-const AddProduct = lazy(()=>import("../../views/seller/AddProduct"))
-const Products = lazy(()=>import("../../views/seller/Products"))
-const DiscountProducts = lazy(()=>import("../../views/seller/DiscountProducts"))
-const Orders = lazy(()=>import("../../views/seller/Orders"))
-const Payments = lazy(()=>import("../../views/seller/Payments"))
-
+import { lazy } from 'react'
+const SellerDashboard = lazy(() => import("../../views/seller/SellerDashboard"))
+const AddProduct = lazy(() => import("../../views/seller/AddProduct"))
+const Products = lazy(() => import("../../views/seller/Products"))
+const DiscountProducts = lazy(() => import("../../views/seller/DiscountProducts"))
+const Orders = lazy(() => import("../../views/seller/Orders"))
+const Payments = lazy(() => import("../../views/seller/Payments"))
+const SellerToAdmin = lazy(() => import("../../views/seller/SellerToAdmin"))
+const SellerToCustomer = lazy(() => import("../../views/seller/SellerToCustomer"))
+const Profile = lazy(() => import("../../views/seller/Profile"))
+const EditProduct = lazy(() => import("../../views/seller/EditProduct"))
+const OrderDetails = lazy(() => import("../../views/seller/OrderDetails"))
+const Pending = lazy(() => import("../../views/Pending"))
+const Deactive = lazy(() => import("../../views/Deactive"))
 export const sellerRoutes = [
+
     {
-        path:"/",
-        element: <Home/>,
-        ability:["admin","seller"]
+        path: '/seller/account-pending',
+        element: <Pending />,
+        ability: 'seller'
     },
     {
-        path:"/seller/dashboard",
-        element: <SellerDashboard/>,
-        ability:["admin","seller"]
+        path: '/seller/account-deactive',
+        element: <Deactive />,
+        ability: 'seller'
     },
+
+    {
+        path: '/seller/dashboard',
+        element: <SellerDashboard />,
+        role: 'seller',
+        status: 'active'
+    },
+
     {
         path: '/seller/dashboard/add-product',
         element: <AddProduct />,
+        role: 'seller',
+        status: 'active'
+    },
+    {
+        path: '/seller/dashboard/edit-product/:productId',
+        element: <EditProduct />,
         role: 'seller',
         status: 'active'
     },
@@ -32,6 +50,7 @@ export const sellerRoutes = [
         role: 'seller',
         status: 'active'
     },
+
     {
         path: '/seller/dashboard/discount-products',
         element: <DiscountProducts />,
@@ -42,7 +61,13 @@ export const sellerRoutes = [
         path: '/seller/dashboard/orders',
         element: <Orders />,
         role: 'seller',
-        status: 'active'
+        visibility: ['active', 'deactive']
+    },
+    {
+        path: '/seller/dashboard/order/details/:orderId',
+        element: <OrderDetails />,
+        role: 'seller',
+        visibility: ['active', 'deactive']
     },
     {
         path: '/seller/dashboard/payments',
@@ -50,4 +75,28 @@ export const sellerRoutes = [
         role: 'seller',
         status: 'active'
     },
+    {
+        path: '/seller/dashboard/chat-support',
+        element: <SellerToAdmin />,
+        role: 'seller',
+        visibility: ['active', 'deactive', 'pending']
+    },
+    {
+        path: '/seller/dashboard/chat-customer/:customerId',
+        element: <SellerToCustomer />,
+        role: 'seller',
+        status: 'active'
+    },
+    {
+        path: '/seller/dashboard/chat-customer',
+        element: <SellerToCustomer />,
+        role: 'seller',
+        status: 'active'
+    },
+    {
+        path: '/seller/dashboard/profile',
+        element: <Profile />,
+        role: 'seller',
+        visibility: ['active', 'deactive', 'pending']
+    }
 ]
